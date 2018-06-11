8b406118354993914d9e9f6b928d5b990c0d56a4
TongYi1 <tongcnsyc@gmail.com>
2018-06-11 13:18:19 +0800

    Initial commit

---
+++ src/main/java/masterspingmvc/MasterspringmvcApplication.java
@@ -0,0 +1,13 @@
+package masterspingmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+public class MasterspringmvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterspringmvcApplication.class, args);
+    }
+}
